e39e44c65542ba776ea83d586af32cfbe608b8cf
mbrohede <marcus.brohede@his.se>
2014-04-04 15:05:17 +0200

    Added comments

--- all_projects.php
+++ all_projects.php
@@ -1,5 +1,9 @@
 <?php
+
+/* Connect to the database */
 include_once('../dbconnect.php');  
+
+
 /* change character set to utf8 */
 if (!mysqli_set_charset($con, "utf8")) {
     echo "Error loading character set utf8: " . mysqli_error($con);
